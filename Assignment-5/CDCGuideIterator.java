d4c7e943f02aacabedd04b6db451c232ba03c633
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 16:37:56 -0500

    Create CDCGuideIterator.java

---
+++ Assignment-5/CDCGuideIterator.java
@@ -0,0 +1,16 @@
+
+public class CDCGuideIterator implements Iterator {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TVListings next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
