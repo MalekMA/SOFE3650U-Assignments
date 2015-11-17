51fb5633d2066c096a230111f1e870bd587c6327
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 21:23:43 -0500

    Update GuideTester.java

--- Assignment-5/GuideTester.java
+++ Assignment-5/GuideTester.java
@@ -1,11 +1,16 @@
-
+/**
+ * Done by: 	Malek Mustapha-Abdullah	100541476
+ *		Clayton Cheung		100539921
+ * Course: 	SOFE3650U Software Design and Architecture
+ * Date: 	November 16, 2015
+ */
+ 
+ // The GuideTester class includes the main method for the program
 public class GuideTester {
 	public static void main(String args[]){
-		
-		CDCGuide DCGuide = new CDCGuide();
-		CUOITGuide UOITGuide = new CUOITGuide();
-		MyTVGuide guide = new MyTVGuide(DCGuide, UOITGuide);
-		guide.printTVListings();
-		
+		CDCGuide DCGuide = new CDCGuide();			// Creates an instance of CDCGuide class
+		CUOITGuide UOITGuide = new CUOITGuide();		// Creates an instance of CUOITGuide class
+		MyTVGuide guide = new MyTVGuide(DCGuide, UOITGuide);	// Creates an instance of MyTVGuide class with DC and UOIT guides as arguments
+		guide.printTVListings();				// Prints the TV Guide
 	}
 }
