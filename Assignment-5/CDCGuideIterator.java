3a134704c371388c52b90cb6d91e3878ce862748
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 20:29:27 -0500

    Update CDCGuideIterator.java

--- Assignment-5/CDCGuideIterator.java
+++ Assignment-5/CDCGuideIterator.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
 
-public class CDCGuideIterator implements Iterator {
 
-	@Override
-	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+public class CDCGuideIterator implements Iterator, java.util.Iterator<TVListings>{
+	
+	ArrayList<TVListings> myListings = new ArrayList<TVListings>();
+	int position = 0;
+	
+	public CDCGuideIterator(ArrayList<TVListings> listings){
+		this.myListings = listings;
 	}
-
+	
 	@Override
 	public TVListings next() {
-		// TODO Auto-generated method stub
-		return null;
+		TVListings listings = myListings.get(position);
+		position++;
+		return listings;
 	}
-
+	
+	@Override
+	public boolean hasNext() {
+		if(position >= myListings.size() || myListings.get(position) == null){
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
