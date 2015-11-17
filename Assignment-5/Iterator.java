5f61c18d7e04e4e1678cc69080e80d458a78f443
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 21:26:20 -0500

    Added documentation

--- Assignment-5/Iterator.java
+++ Assignment-5/Iterator.java
@@ -1,5 +1,16 @@
-
+/**
+ * Done by: 	Malek Mustapha-Abdullah	100541476
+ *		Clayton Cheung		100539921
+ * Course: 	SOFE3650U Software Design and Architecture
+ * Date: 	November 16, 2015
+ */
+ 
 public interface Iterator {
+	
+	// The hasNext method is provided in order to check if the data structures are empty 
+	// and to know when the end of the data structure is reached
 	public boolean hasNext();
+	
+	// The next method is provided in order to obtain the arguments inside the data structures
 	public TVListings next();
 }
