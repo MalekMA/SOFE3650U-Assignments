7ebeb15a9b41d5d64817f24c7ce961aa464260d3
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 21:21:18 -0500

    Added documentation

--- Assignment-5/Guide.java
+++ Assignment-5/Guide.java
@@ -1,5 +1,11 @@
+/**
+ * Done by: 	Malek Mustapha-Abdullah	100541476
+ *		Clayton Cheung		100539921
+ * Course: 	SOFE3650U Software Design and Architecture
+ * Date: 	November 16, 2015
+ */
+ 
 import java.util.Iterator;
-
 
 public interface Guide {
 	public Iterator<TVListings> createIterator();
