0901a9be1f9b07f9425dcf441c5bbb9ec7de6ef6
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 21:41:13 -0500

    Added documentation

--- Assignment-5/TVListingsInt.java
+++ Assignment-5/TVListingsInt.java
@@ -1,5 +1,15 @@
-
+/**
+ * Done by: 	Malek Mustapha-Abdullah	100541476
+ *		Clayton Cheung		100539921
+ * Course: 	SOFE3650U Software Design and Architecture
+ * Date: 	November 16, 2015
+ */
+ 
 public interface TVListingsInt {
+	
+	// This method is used to return the title of a listing
 	public String getTitle();
+	
+	// This method is used to return the time of a listing
 	public String getTime();
 }
