7d17a8894c80c991f05507add0aae4a66381a3ab
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-16 20:31:44 -0500

    Update TVListings.java

--- Assignment-5/TVListings.java
+++ Assignment-5/TVListings.java
@@ -1,2 +1,21 @@
-public interface TVListings extends TVListingsInt {
+
+public class TVListings implements TVListingsInt {
+	
+	String title;
+	String time;
+	
+	public TVListings(String title, String time){
+		this.title = title;
+		this.time = time;
+	}
+	
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public String getTime() {
+		return time;
+	}
 }
